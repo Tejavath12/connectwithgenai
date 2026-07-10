1c7d3e8774dcbe87b7bb2a82237c2c8401c6d406
Tejavath Pallavi <tejavathpallavi064@gmail.com>
2026-07-10 19:53:41 +0530

    Update internship.py

--- internship.py
+++ internship.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AQ.Ab8RN6LE-aJFbT16HNuHo6lUJ76CdNqrBY4FonnjsL6EdKezRA")
+client = genai.Client(api_key="")
 
 for model in client.models.list():
     print(model.name)
